7f80ba199c4faffc4ca0e44b648cea6b9ded8aa8
hyoi <hyoi.kaz@gmail.com>
2022-01-12 21:14:26 +0900

    .wasmファイル名修正

--- target/wasm.js
+++ target/wasm.js
@@ -328,7 +328,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('tigtag_bg.wasm', import.meta.url);
+        input = new URL('wasm_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
