1b6b3c2764f6150d1cedfb7b34b45c96f82e6b42
hyoi <hyoi.kaz@gmail.com>
2025-08-08 23:13:01 +0900

    ソースコードのマイナーな変更

--- crates/tigtag/src/main.rs
+++ crates/tigtag/src/main.rs
@@ -11,10 +11,8 @@ use bevy::{
     asset::{LoadedUntypedAsset, LoadState},
     render::camera::Viewport,
     diagnostic::{FrameTimeDiagnosticsPlugin, DiagnosticsStore},
-    input::{
-        gamepad::{
-            GamepadInput, GamepadButton::*, GamepadAxis::*, GamepadAxisChangedEvent,
-        },
+    input::gamepad::{
+        GamepadInput, GamepadButton::*, GamepadAxis::*, GamepadAxisChangedEvent,
     },
     // ecs::query::QueryFilter,
 };
@@ -27,8 +25,6 @@ use std::{
     ops::{Range, Deref, DerefMut, Add, AddAssign},
     f32::consts::{PI, TAU},
 };
-
-//------------------------------------------------------------------------------
 
 // proc-macro
 use macros::MyState;
@@ -56,148 +52,134 @@ fn main() -> AppExit
     );
 
     // アプリを生成
-    let mut appl = App::new();
+    let mut application = App::new();
 
-    // Resourceを登録
-    appl.init_resource::<Record>()   // ゲームの成績
+    //各種登録・準備
+    application
+        // アプリを初期化しassetsをロードする
+        .add_plugins(init_app::Schedule)
+        .add_plugins(load_assets::Schedule)
+        .insert_resource(load_assets::NextState(MyState::InitGame)) // ロード完了後の遷移先State
+        // Resourceを登録
+        .init_resource::<Record>() // ゲームの成績
         .init_resource::<map::Map>() // ステージのマップ
         .init_resource::<player::PlayerInput>() // プレイヤーの入力
         .insert_resource(player::KeyMap(FxHashMap::from_iter(KEY_MAP))) //マッピング（キー）
         .insert_resource(player::PadMap(FxHashMap::from_iter(PAD_MAP))) //マッピング（ゲームパッド）
+        // Eventを登録
+        .add_event::<EventStageClear>() //ステージクリアの伝達
+        .add_event::<EventGameOver>() //ゲームオーバーの伝達
+        // .add_event::<EventTimerPlayer>()  //プレイヤー移動タイマーのfinishedの伝達
+        // .add_event::<EventEatDot>() //スコアリングの伝達
+        // .add_event::<EventTimerChasers>() //敵キャラ移動タイマーのfinishedの伝達
         ;
 
-    // Eventを登録
-    appl.add_event::<EventStageClear>()  //ステージクリアの伝達
-        .add_event::<EventGameOver>()    //ゲームオーバーの伝達
-    // .add_event::<EventTimerPlayer>()  //プレイヤー移動タイマーのfinishedの伝達
-    // .add_event::<EventEatDot>() //スコアリングの伝達
-    // .add_event::<EventTimerChasers>() //敵キャラ移動タイマーのfinishedの伝達
-        ;
-
-    // アプリを初期化しassetsをロードする
-    appl.add_plugins(init_app::Schedule)
-        .add_plugins(load_assets::Schedule)
-        .insert_resource(load_assets::NextState(MyState::InitGame)); // 完了後の遷移先
-
-    //----------------------------------------------------------------------
     // Updateスケジュール（without State）
-    //----------------------------------------------------------------------
+    // State関係なしの処理（例えばPause中も動作し続ける処理）
+    application
+        .insert_resource(header_info::PlaceHolder(PLACE_HOLDER)) // 表示位置
+        .insert_resource(PlaceHolderFps(header_footer::BottomLeft, 1)) // 表示位置
+        .add_plugins(FrameTimeDiagnosticsPlugin::default()) // FPS Plugin
+        .add_systems(
+            Update,
+            (
+                // ゲームパッドの接続状態を検出する
+                misc::detect_gamepad_connection,
+                //ヘッダーの表示情報を更新する
+                header_info::update::<header_info::PlaceHolder>,
+                //フッターの表示情報を更新する
+                update_fps::<PlaceHolderFps>,
+                //スプライトアニメーション
+                animate_sprites::<player::Player>, // プレイヤー
+                animate_sprites::<chaser::Chaser>, // チェイサー
+                // スプライト表示OFFの場合のアニメーション
+                chaser::rotate_chaser_shape.run_if(SPRITE_OFF), // チェイサー回転
+                // UI Nodeのアウトラインの表示／非表示を切替える
+                misc::toggle_ui_outline_gizmo.run_if(DEBUG),
+            ),
+        )
+        // アプリ終了キーをフックして処理を挿入
+        // appl.add_systems(
+        //     Update,
+        //     hook_exit_app_key // Pause等の雛型
+        //         .before(misc::app_close_on_key),
+        // )
+        ;
 
-    // ゲームパッドの接続状態を検出する
-    appl.add_systems(Update, misc::detect_gamepad_connection);
-
-    //ヘッダーの表示を更新する
-    appl.insert_resource(header_info::PlaceHolder(PLACE_HOLDER)) // 表示位置
-        .add_systems(Update, header_info::update::<header_info::PlaceHolder>); // 表示の更新
-
-    //フッターの表示を更新する
-    appl.insert_resource(PlaceHolderFps(header_footer::BottomLeft, 1)) // 表示位置
-        .add_systems(Update, update_fps::<PlaceHolderFps>) // 表示の更新
-        .add_plugins(FrameTimeDiagnosticsPlugin::default()); // FPS Plugin
-
-    // スプライトアニメーション
-    appl.add_systems(
-        Update,
-        (
-            //スプライトアニメーション
-            animate_sprites::<player::Player>, // プレイヤー
-            animate_sprites::<chaser::Chaser>, // チェイサー
-            // スプライト表示OFFの場合
-            chaser::rotate_chaser_shape.run_if(SPRITE_OFF), // チェイサー回転
-        ),
-    );
-
-    // アプリ終了キーをフックして処理を挿入
-    // appl.add_systems(
-    //     Update,
-    //     hook_exit_app_key // Pause等の雛型
-    //         .before(misc::app_close_on_key),
-    // );
-
-    // UI Nodeのアウトラインの表示／非表示を切替える
-    appl.add_systems(Update, misc::toggle_ui_outline_gizmo.run_if(DEBUG));
-
-    //----------------------------------------------------------------------
     // MyState::InitGameスケジュール
-    //----------------------------------------------------------------------
-
-    // カメラをspawnする
-    appl.insert_resource(simple_camera::Settings(CAMERA_SETTINGS.clone()))
+    // ゲームの初期化
+    application
+        .insert_resource(simple_camera::Settings(CAMERA_SETTINGS.clone()))
         .add_systems(
             OnEnter(MyState::InitGame),
             (
-                simple_camera::spawn::<simple_camera::Settings>,
+                // カメラをspawnする
+                simple_camera::spawn::<simple_camera::Settings>
+                    .before(misc::select_ui_camera),
                 misc::select_ui_camera, // UIを描画するカメラを選ぶ
-            )
-                .chain(), // 実行順の固定
-        );
-
-    // 無条件遷移
-    appl.add_systems(OnEnter(MyState::InitGame), set_next_state::<StageStart>);
-    // appl.add_systems(OnEnter(MyState::InitGame), set_next_state::<TitleDemo>);
-
-    // ヘッダー／フッターの準備
-    appl.insert_resource(header_footer::Settings(HEADER_FOOTER)) // UIの情報（Resource）
+                // 無条件遷移
+                set_next_state::<StageStart>,
+            ),
+        )
+        .insert_resource(header_footer::Settings(HEADER_FOOTER)) // UIの情報
+        .init_resource::<PopupMessages>() // ポップアップメッセージの情報
         .add_systems(
             OnExit(MyState::InitGame),
-            header_footer::spawn_header_footer, // UIをspawnする
+            (
+                // ヘッダー／フッターの準備
+                header_footer::spawn_header_footer,
+                // ポップアップメッセージの準備
+                popup_messages::spawn::<PopupMessages>,
+            ),
         );
 
-    // ポップアップメッセージの準備
-    appl.init_resource::<PopupMessages>()
-        .add_systems(OnExit(MyState::InitGame), popup_messages::spawn::<PopupMessages>);
-
-    //----------------------------------------------------------------------
     // MyState::StageStartスケジュール
-    //----------------------------------------------------------------------
-
-    // ステージ初期化
-    appl.add_systems(
-        OnEnter(MyState::StageStart),
-        (
+    // ステージ開始の処理
+    application
+        .add_systems(
+            OnEnter(MyState::StageStart),
             (
-                //マップデータ生成
-                map::make_new_stage_data,
-                //スプライトのspawn
                 (
-                    map::spawn_sprite,
-                    player::spawn_sprite,
-                    chaser::spawn_sprite,
+                    //マップデータ生成
+                    map::make_new_stage_data,
+                    //スプライトのspawn
+                    (
+                        map::spawn_sprite,
+                        player::spawn_sprite,
+                        chaser::spawn_sprite,
+                    )
+                        .after(map::make_new_stage_data),
+                    // 無条件遷移
+                    set_next_state::<MainLoop>, //★★★DEBUG後に削除すること★★★
                 ),
-                // 無条件遷移
-                set_next_state::<MainLoop>, //★★★DEBUG後に削除すること★★★
-            )
-                .chain(), //実行順の固定
-            //TextUIの可視化
-            (
-                // effect::init_count::<stage_start::CountDown>, //カウント初期化
-                misc::show_component::<popup::StageSatrt>,
-            )
-                .chain(), //実行順の固定
-        ),
-        /*
-        // )
-        // .add_systems
-        // (   Update,
-        //     (   //TextUIの演出
+                //TextUIの可視化
+                (
+                    // effect::init_count::<stage_start::CountDown>, //カウント初期化
+                    misc::show_component::<popup::StageSatrt>,
+                )
+                    .chain(), //実行順の固定
+            ),
+        )
+        // .add_systems(
+        //     Update,
+        //     (
+        //         //TextUIの演出
         //         effect::count_down::<stage_start::CountDown>, //カウントダウン
         //     )
-        //     .run_if( in_state( MyState::StageStart ) )
+        //         .run_if(in_state(MyState::StageStart)),
         // )
-        // .add_systems
-        // (   OnExit ( MyState::StageStart ),
-        //     (   //TextUIの不可視化
+        // .add_systems(
+        //     OnExit(MyState::StageStart),
+        //     (
+        //         //TextUIの不可視化
         //         misc::hide_component::<stage_start::Message>,
-        //     )
-         */
-    );
+        //     ),
+        // )
+        ;
 
-    //----------------------------------------------------------------------
     // MyState::MainLoopスケジュール
-    //----------------------------------------------------------------------
-
     // メインループ
-    appl.add_systems(
+    application.add_systems(
         Update,
         (
             // スコアリング＆クリア判定
@@ -228,77 +210,72 @@ fn main() -> AppExit
             .run_if(in_state(MyState::MainLoop)),
     );
 
-    //----------------------------------------------------------------------
     // MyState::StageClearスケジュール
-    //----------------------------------------------------------------------
-
     // ステージクリアの処理
-    appl.add_systems(
-        OnEnter(MyState::StageClear),
-        // 無条件遷移
-        set_next_state::<StageStart>, //★★★DEBUG後に削除すること★★★
-    );
-    // appl.add_systems
-    // (   OnEnter ( MyState::StageClear ),
-    //     (   //TextUIの可視化
-    //         effect::init_count::<stage_clear::CountDown>, //カウント初期化
-    //         misc::show_component::<stage_clear::Message>,
-    //     )
-    //     .chain(), //実行順の固定
-    // )
-    // .add_systems
-    // (   Update,
-    //     (   //TextUIの演出
-    //         effect::count_down::<stage_clear::CountDown>, //カウントダウン
-    //     )
-    //     .run_if( in_state( MyState::StageClear ) )
-    // )
-    // .add_systems
-    // (   OnExit ( MyState::StageClear ),
-    //     (   //TextUIの不可視化
-    //         misc::hide_component::<stage_clear::Message>,
-    //     )
-    // );
+    application
+        .add_systems(
+            OnEnter(MyState::StageClear),
+            // 無条件遷移
+            set_next_state::<StageStart>, //★★★DEBUG後に削除すること★★★
+        )
+        // appl.add_systems
+        // (   OnEnter ( MyState::StageClear ),
+        //     (   //TextUIの可視化
+        //         effect::init_count::<stage_clear::CountDown>, //カウント初期化
+        //         misc::show_component::<stage_clear::Message>,
+        //     )
+        //     .chain(), //実行順の固定
+        // )
+        // .add_systems
+        // (   Update,
+        //     (   //TextUIの演出
+        //         effect::count_down::<stage_clear::CountDown>, //カウントダウン
+        //     )
+        //     .run_if( in_state( MyState::StageClear ) )
+        // )
+        // .add_systems
+        // (   OnExit ( MyState::StageClear ),
+        //     (   //TextUIの不可視化
+        //         misc::hide_component::<stage_clear::Message>,
+        //     )
+        // )
+        ;
 
-    //----------------------------------------------------------------------
     // MyState::GameOverスケジュール
-    //----------------------------------------------------------------------
-
     // ゲームオーバーの処理
-    appl.add_systems(
-        OnEnter(MyState::GameOver),
-        // 無条件遷移
-        set_next_state::<StageStart>, //★★★DEBUG後に削除すること★★★
-    );
-    // appl.add_systems
-    // (   OnEnter ( MyState::GameOver ),
-    //     (   //TextUIの可視化
-    //         effect::init_count::<game_over::CountDown>,//カウント初期化
-    //         misc::show_component::<game_over::Message>,
-    //     )
-    //     .chain() //実行順の固定
-    // )
-    // .add_systems
-    // (   Update,
-    //     (   //TextUIの演出＆入力待ち
-    //         effect::count_down::<game_over::CountDown>, //カウントダウン
-    //         effect::blinking_text::<game_over::TextREPLAY>, //Replay? の明滅
-    //         effect::hit_any_key::<StageStart>, //Hit ANY Key
-    //     )
-    //     .run_if( in_state( MyState::GameOver ) )
-    // )
+    application
+        .add_systems(
+            OnEnter(MyState::GameOver),
+            // 無条件遷移
+            set_next_state::<StageStart>, //★★★DEBUG後に削除すること★★★
+        )
+        // appl.add_systems
+        // (   OnEnter ( MyState::GameOver ),
+        //     (   //TextUIの可視化
+        //         effect::init_count::<game_over::CountDown>,//カウント初期化
+        //         misc::show_component::<game_over::Message>,
+        //     )
+        //     .chain() //実行順の固定
+        // )
+        // .add_systems
+        // (   Update,
+        //     (   //TextUIの演出＆入力待ち
+        //         effect::count_down::<game_over::CountDown>, //カウントダウン
+        //         effect::blinking_text::<game_over::TextREPLAY>, //Replay? の明滅
+        //         effect::hit_any_key::<StageStart>, //Hit ANY Key
+        //     )
+        //     .run_if( in_state( MyState::GameOver ) )
+        // )
+        //scoreとstageをゼロクリアする
+        .add_systems(OnExit(MyState::GameOver), initialize_record_except_hi_score)
+        //     (   //TextUIの不可視化
+        //         misc::hide_component::<game_over::Message>,
+        ;
 
-    //scoreとstageをゼロクリアする
-    appl.add_systems(OnExit(MyState::GameOver), initialize_record_except_hi_score);
-    //     (   //TextUIの不可視化
-    //         misc::hide_component::<game_over::Message>,
-
-    //----------------------------------------------------------------------
     // MyState::TitleDemoスケジュール
-    //----------------------------------------------------------------------
-
-    // タイトル画面
-    // appl.add_systems
+    // タイトル画面の処理
+    // application
+    // .add_systems
     // (   OnEnter ( MyState::TitleDemo ),
     //     (   //TextUIの可視化
     //         misc::show_component::<title_demo::Message>,
@@ -316,16 +293,13 @@ fn main() -> AppExit
     // (   OnExit ( MyState::TitleDemo ),
     //     (   //TextUIの不可視化
     //         misc::hide_component::<title_demo::Message>,
-
     //         //scoreとstageをゼロクリアする(DEMOでステージクリアの時はしない)
     //         initialize_record_except_hi_score,
     //     )
     // );
 
-    //----------------------------------------------------------------------
-
     // アプリを実行
-    appl.run()
+    application.run()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
