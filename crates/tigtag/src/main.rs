bf73c194e7c18f2783c9f1cfbc3e9b03ac1a235f
hyoi <hyoi.kaz@gmail.com>
2025-09-26 22:15:22 +0900

    初回コミット

--- crates/tigtag/src/main.rs
+++ crates/tigtag/src/main.rs
@@ -2,51 +2,51 @@
 use bevy::{
     prelude::*,
     ecs::{
-        error::{GLOBAL_ERROR_HANDLER, warn},
-        system::SystemParam,
-        component::Mutable,
+        error::{/*GLOBAL_ERROR_HANDLER,*/ warn},
+    //     system::SystemParam,
+    //     component::Mutable,
     },
-    window::{EnabledButtons, WindowMode},
-    log::LogPlugin,
-    diagnostic::{FrameTimeDiagnosticsPlugin, DiagnosticsStore},
-    color::palettes::*,
-    asset::{LoadedUntypedAsset, LoadState},
-    render::camera::Viewport,
-    input::{
-        keyboard::NativeKeyCode,
-        gamepad::{GamepadInput, GamepadAxisChangedEvent},
-    },
-    audio::Volume,
+    // window::{EnabledButtons, WindowMode},
+    // log::LogPlugin,
+    // diagnostic::{FrameTimeDiagnosticsPlugin, DiagnosticsStore},
+    // color::palettes::*,
+    // asset::{LoadedUntypedAsset, LoadState},
+    // render::camera::Viewport,
+    // input::{
+    //     keyboard::NativeKeyCode,
+    //     gamepad::{GamepadInput, GamepadAxisChangedEvent},
+    // },
+    // audio::Volume,
 };
-use rand::prelude::*;
-use rustc_hash::{FxHashMap, FxHashSet};
+// use rand::prelude::*;
+// use rustc_hash::{FxHashMap, FxHashSet};
 
 // standard library
-use std::{
-    slice::Iter,
-    ops::{Range, Deref, DerefMut, Add, AddAssign},
-    f32::consts::{TAU, PI},
-    collections::VecDeque,
-};
+// use std::{
+//     slice::Iter,
+//     ops::{Range, Deref, DerefMut, Add, AddAssign},
+//     f32::consts::{TAU, PI},
+//     collections::VecDeque,
+// };
 
 // proc-macro
-use macros::MyState;
-use macros::{OverlayMessage, Blinking, CountDown};
-use macros::{OverlayMenu, ScalingItem};
-use macros::derive_appctrl_input;
+// use macros::MyState;
+// use macros::{OverlayMessage, Blinking, CountDown};
+// use macros::{OverlayMenu, ScalingItem};
+// use macros::derive_appctrl_input;
 
 // internal submodules
-mod my_utils; // 共通ライブラリ
-use my_utils::*;
+// mod my_utils; // 共通ライブラリ
+// use my_utils::*;
 
-mod config; // 設定各種
-use config::*;
+// mod config; // 設定各種
+// use config::*;
 
-mod core_logic; // ゲームロジック
-use core_logic::*;
-use core_logic::overlay_ui::OverlayMessage;
+// mod core_logic; // ゲームロジック
+// use core_logic::*;
+// use core_logic::overlay_ui::OverlayMessage;
 
-mod demo_play; // demoロジック
+// mod demo_play; // demoロジック
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -54,12 +54,18 @@ mod demo_play; // demoロジック
 fn main() -> AppExit
 {
     // エラーハンドラ設定
-    GLOBAL_ERROR_HANDLER.set(warn).expect(
-        "Configure the error handler in main() once, prior to app initialization.",
-    );
+    // GLOBAL_ERROR_HANDLER.set(warn).expect(
+    //     "Configure the error handler in main() once, prior to app initialization.",
+    // );
 
-    // アプリ実行
-    App::new().add_plugins(core_logic::MainSchedule).run()
+    // アプリの生成
+    App::new()
+        // エラーハンドラ設定
+        .set_error_handler(warn)
+        // メインスケジュール
+        // .add_plugins(core_logic::MainSchedule)
+        // アプリ実行
+        .run()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
